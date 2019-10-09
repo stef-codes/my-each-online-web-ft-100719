7594413a966fa84c85b75a973d1cafab98eb446d
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 05:26:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
 def my_each(array) 
-  
+  i = 0 
+  while i < array.length 
+    yield array[i]
+    i += 1
+  end 
+  array
 end
+
+=begin
+def hello_t(array)
+  if block_given?
+  i = 0 
+  while i < array.length 
+    yield array[i]
+  i+= 1 
+  end 
+    array
+  else 
+    puts "Hey! No block was given!"
+  end
+end
+=end
